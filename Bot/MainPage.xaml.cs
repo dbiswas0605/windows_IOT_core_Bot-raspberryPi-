6ac1931b63c3aeba92eaf2abb6e22973c86150e9
dbiswas0605 <debu.gemini@gmail.com>
2019-12-20 23:00:28 -0600

    Add project files.

---
+++ Bot/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using BOT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Bot
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MyBot mybot;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            mybot = new MyBot();
+        }
+
+        private async void ButtonUP_Click(object sender, RoutedEventArgs e)
+        {
+            Direction.Text = "UP";
+            mybot.moveForward();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            mybot.stop();
+        }
+
+        private async void ButtonRight_Click(object sender, RoutedEventArgs e)
+        {
+            Direction.Text = "RIGHT";
+            mybot.turnRight();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            mybot.stop();
+        }
+
+        private async void ButtonDown_Click(object sender, RoutedEventArgs e)
+        {
+            Direction.Text = "DOWN";
+            mybot.moveBackward ();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            mybot.stop();
+        }
+
+        private async void ButtonLeft_Click(object sender, RoutedEventArgs e)
+        {
+            Direction.Text = "LEFT";
+            mybot.turnLeft();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            mybot.stop();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Direction.Text = "stop";
+            mybot.stop();
+        }
+    }
+}
---
+++ Bot/MyBot.cs
@@ -0,0 +1,143 @@
+﻿using Windows.Devices.Gpio;
+
+namespace BOT
+{
+    public sealed class MyBot
+    {
+        // Get the default GPIO controller on the system
+        GpioController gpio;
+
+        const int PIN_MOTOR_LEFT_FORWARD = 27;
+        const int PIN_MOTOR_LEFT_BACKWARD = 22;
+
+        const int PIN_MOTOR_RIGHT_FORWARD = 23;
+        const int PIN_MOTOR_RIGHT_BACKWARD = 24;
+
+        GpioPin leftwheelF, leftwheelB;
+        GpioPin rightwheelF, rightwheelB;
+
+        public MyBot()
+        {
+            // Get the default GPIO controller on the system
+            gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+                return; // GPIO not available on this system
+
+            leftwheelF = gpio.OpenPin(PIN_MOTOR_LEFT_FORWARD);
+            leftwheelB = gpio.OpenPin(PIN_MOTOR_LEFT_BACKWARD);
+
+            rightwheelF = gpio.OpenPin(PIN_MOTOR_RIGHT_FORWARD);
+            rightwheelB = gpio.OpenPin(PIN_MOTOR_RIGHT_BACKWARD);
+
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.Low);
+
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.Low);
+
+            leftwheelF.SetDriveMode(GpioPinDriveMode.Output);
+            leftwheelB.SetDriveMode(GpioPinDriveMode.Output);
+
+            rightwheelF.SetDriveMode(GpioPinDriveMode.Output);
+            rightwheelB.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private void leftWheelGoForward()
+        {
+            leftwheelF.Write(GpioPinValue.High);
+            leftwheelB.Write(GpioPinValue.Low);
+        }
+
+        private void rightWheelGoForward()
+        {
+            rightwheelF.Write(GpioPinValue.High);
+            rightwheelB.Write(GpioPinValue.Low);
+        }
+
+        private void leftWheelGoBackward()
+        {
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.High);
+        }
+
+        private void rightWheelGoBackward()
+        {
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.High);
+        }
+
+        private void leftWheelStop()
+        {
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.Low);
+        }
+
+        private void rightWheelStop()
+        {
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.Low);
+        }
+
+        public void exit()
+        {
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.Low);
+
+            leftwheelB.Dispose();
+            leftwheelF.Dispose();
+
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.Low);
+
+            rightwheelB.Dispose();
+            rightwheelF.Dispose();
+        }
+
+        public void moveBackward()
+        {
+            leftWheelGoForward();
+            rightWheelGoForward();
+        }
+
+        public void moveForward()
+        {
+            leftWheelGoBackward();
+            rightWheelGoBackward();
+        }
+
+        public void stop()
+        {
+            rightWheelStop();
+            leftWheelStop();
+        }
+
+        public void turnLeft()
+        {
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.Low);
+
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.Low);
+
+            rightWheelGoForward();
+            //await Task.Delay(TimeSpan.FromSeconds(2));
+            leftWheelGoBackward();
+            //await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+
+        public void turnRight()
+        {
+            leftwheelF.Write(GpioPinValue.Low);
+            leftwheelB.Write(GpioPinValue.Low);
+
+            rightwheelF.Write(GpioPinValue.Low);
+            rightwheelB.Write(GpioPinValue.Low);
+
+            rightWheelGoBackward();
+            //await Task.Delay(TimeSpan.FromSeconds(2));
+            leftWheelGoForward();
+            //await Task.Delay(TimeSpan.FromSeconds(2));
+        }
+    }
+}
